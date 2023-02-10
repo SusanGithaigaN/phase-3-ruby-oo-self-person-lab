14dea76e8a9426b37db313a5bf0d19c282e78880
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-10 21:22:53 +0300

    First commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+    # happiness conditions
+    # if happiness>10 = no change
+    # if happiness < 0 = no change
+    def happiness(num)
+        if num > 10
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        else
+            @happiness = num
+        end
+    end
+    
+
+  
+end   
