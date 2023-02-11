d5774fe88cf343ca139edba9e0c4908b4dcdda30
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-11 04:50:19 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 class Person 
+    # name not overwritten: use attr_reader
     attr_reader :name
     attr_accessor :bank_account, :happiness, :hygiene
     def initialize (name)
@@ -31,6 +32,8 @@ class Person
       end
     end
     # happy
+    # if happiness > 7 return true, else return false
+    # ternary operator happiness > 7 ? true :fale
     def happy?
         self.happiness > 7 ? true :false
     end
@@ -41,20 +44,43 @@ class Person
     # get paid
     def get_paid(amount)
         self.bank_account += amount
-        puts "all about the benjamins"
+        "all about the benjamins"
     end
     # take_bath
     def take_bath
         self.hygiene += 4
-        puts "♪ Rub-a-dub just relaxing in the tub ♫"
+        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
     # work_out
     def work_out
         self.hygiene -= 3
         self.happiness += 2
-        puts "♪ another one bites the dust ♫"
+        "♪ another one bites the dust ♫"
     end
     # def call_friend
+    # call happier: happiness + 3 points
+    # make friend happier: friend.happiness
+    # return string
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    # start_conver..
+    # person, topic: start convo with ? person(friend) , topic
+    # loop through convo (if, else statement)
+    def start_conversation(friend, topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+            'blah blah partisan blah lobbyist' 
+        elsif topic == 'weather'
+            self.happiness += 1
+            friend.happiness += 1
+             'blah blah sun blah rain'
+        else 
+             'blah blah blah blah blah' 
+        end
+    end
 
-  
 end   
